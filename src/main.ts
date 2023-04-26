1ce298742eb02c270d954c7e29d2b65416422f5a
Jhordan Oliveira <jhordanjes@gmail.com>
2023-04-26 05:39:30 -0400

    port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3003);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
